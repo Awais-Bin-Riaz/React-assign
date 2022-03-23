662721a9ce459785928a2879904128822d03ecea
Awais-Bin-Riaz <awaisbinriaz6@gmail.com>
2022-03-23 16:31:54 +0500

    eidted

--- 3. wheather_app/src/App.js
+++ 3. wheather_app/src/App.js
@@ -1,126 +1,41 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import sun from './sun.png';
-import moon from './moon.png';
-// import backgroundVideo from './video.mp4'
-
-
-{/* <video autoPlay loop muted id='video'>
-  <source src='https://giphy.com/embed/3oEjHB1EKuujDjYFWw' type='video/gif' />
-
-
-</video> */}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [click, setClick] = useState(true);
-  const [weather, setWeather] = useState(null);
-  const [city, setcity] = useState(null);
-  const [mode, setMode] = useState(true);
-  let date = new Date();
-  let time = date.getHours();
-  // console.log(time);
-  let lat, long;
-  function change() {
-    let icon = document.getElementById('icon');
-    let body = document.querySelector('body');
-    icon.src = mode ? sun : moon;
-    body.className = mode ? "Dark" : "Light";
-    icon.alt = mode ? "Sun" : "Moon";
-    setMode(!mode);
-  }
+  const [myWeather, setMyWeather] = useState(null);
+
   useEffect(() => {
-    let icon = document.getElementById('icon');
-    let body = document.querySelector('body');
-    if (time >= 19 || time < 6) {
-      body.className = "Dark";
-      icon.src = sun;
-      icon.alt = "Sun";
-      setMode(false);
-    }
-    else {
-      body.className = "Light";
-      icon.src = moon;
-      icon.alt = "Moon";
-      setMode(true);
-    }
-    if (city != null) {
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e1c32dd0eb29cd3b266f3b06ff7c70b4&units=metric`)
-        .then((res) => {
-          const newWeather = res.data;
-          setWeather(newWeather);
-        }).catch((err) => { alert(`Something went wrong!\n${err}`) })
-    }
-    else {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-        alert("Geolocation is not supported by this browser.");
-      }
-      function showPosition(position) {
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=e1c32dd0eb29cd3b266f3b06ff7c70b4&units=metric`)
-          .then((res) => {
-            const newWeather = res.data;
-            // console.log(newWeather);
-            setWeather(newWeather);
-          }).catch((err) => { alert(`Something went wrong!\n${err}`) })
-        // console.log(lat,long);
-      }
-    }
-  }, [click, city, lat, long])
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=e1c32dd0eb29cd3b266f3b06ff7c70b4&units=metric`
+      )
+      .then((res) => {
+        const newWeather = res.data;
+        console.log(newWeather);
+        setMyWeather(newWeather);
+      })
+      .catch((err) => {
+        alert(`Something went wrong!\n${err}`);
+      });
+  }, []);
 
-  if (weather != null) {
-    return (<div>
-      <div className="div2">
-        <h1>City Weather App</h1>
-        <input placeholder=" city name" id="city" />
-        <button onClick={() => {
-          setcity(document.getElementById('city').value);
-          setClick(!click);
-        }}>Check</button>
-        <img src={moon} alt="Moon" onClick={change} id="icon" />
+  if (myWeather != null) {
+    return (
+      <div>
+        <div className="div2">
+        </div>
+        <div className="div1">
+          <h1>City: {myWeather.name}</h1>
+        </div>
       </div>
-      <div className="div1">
-        <h1>{weather.name}</h1>
-        <p id="desc">{weather.weather[0].description}</p>
-        
-        <h1 id="temp">{weather.main.temp}'C</h1>
-        <p>
-          <span>Latitude: {weather.coord.lat}</span>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <span>Longitude: {weather.coord.lon}</span>
-        </p>
-      </div>
-      <ul>
-        <li>Country : {weather.sys.country}</li>
-        <li>Feels Like : {weather.main.feels_like}'C</li>
-        <li>Humidity : {weather.main.humidity}g/kg</li>
-        <li>Pressure : {weather.main.pressure} Pa</li>
-        <li>Max Temperature : {weather.main.temp_max}'C</li>
-        <li>Min Temperature : {weather.main.temp_min}'C</li>
-        <li>Wind Direction : {weather.wind.deg} degree</li>
-        <li>Wind Speed : {weather.wind.speed}m/s</li>
-      </ul>
-    </div>)
+    );
   }
-  else {
-    return (<div>
-      <div className="div2">
-        <h1>City Weather App</h1>
-        <input placeholder=" city name" id="city" />
-        <button onClick={() => {
-          setcity(document.getElementById('city').value);
-          setClick(!click);
-        }}>check</button>
-        <img src={moon} alt="Moon" onClick={change} id="icon" />
-      </div>
-    </div>)
+   else {
+    return (
+      <></>
+    );
   }
-
-
 }
-
 
 export default App;
